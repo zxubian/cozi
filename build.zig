266b7788f23b01a50e7fcc8d5b43110c89686af9
Andrey Kirpach <andrey.kirpach@gmail.com>
2024-12-25 16:40:46 +0700

    feat(build): support filtering & debugging tests

--- build.zig
+++ build.zig
@@ -26,7 +26,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-        .sanitize_thread = sanitize == .thread,
+        .sanitize_thread = true,
         .use_llvm = true,
     });
 
@@ -57,11 +57,17 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    const test_filter_option = b.option([]const []const u8, "test-filter", "Test filter");
+
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        .filters = if (test_filter_option) |o| o else &.{},
     });
+
+    const install_test_step =
+        b.addInstallArtifact(exe_unit_tests, .{ .dest_dir = .{ .override = .{ .custom = "test" } } });
 
     addAssemblyForMachineContext(b, exe_unit_tests, &target);
 
@@ -69,5 +75,7 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
+
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&install_test_step.step);
 }
