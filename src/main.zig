fe0f87dcb52fee1ad8a0aeabd1225f4852469b0e
Andrey Kirpach <andrey.kirpach@gmail.com>
2025-01-19 23:56:18 +0900

    refactor(main): use lower case for exports that are not types

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,11 @@
 const std = @import("std");
 
 pub const Await = @import("./await/main.zig");
-pub const Containers = @import("/containers/main.zig");
-pub const Core = @import("./core/main.zig");
+pub const containers = @import("/containers/main.zig");
+pub const core = @import("./core/main.zig");
 pub const Coroutine = @import("./coroutine/main.zig");
-pub const Executors = @import("./executors/main.zig");
+pub const executors = @import("./executors/main.zig");
+pub const fault = @import("./fault/main.zig");
 pub const Fiber = @import("./fiber/main.zig");
-pub const Io = @import("./io/");
-pub const Sync = @import("./sync/");
+pub const io = @import("./io/");
+pub const sync = @import("./sync/");
