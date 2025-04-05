f85ec0d0991f00a0199cb226f04e094b1dd1976e
Andrey Kirpach <andrey.kirpach@gmail.com>
2025-04-05 19:37:06 +0900

    optimize(future/lazy): make input value comptime known

--- src/future/lazy/make/value.zig
+++ src/future/lazy/make/value.zig
@@ -10,36 +10,38 @@ const State = future.State;
 const model = future.model;
 const meta = future.meta;
 
-fn Value(V: type) type {
+fn Value(comptime v: anytype) type {
+    const V = @TypeOf(v);
     return struct {
         pub const ValueType = V;
-        value: V,
 
         pub fn Computation(Continuation: anytype) type {
             return struct {
                 next: Continuation,
-                value: V,
+                input: void = undefined,
 
                 pub fn start(self: *@This()) void {
-                    self.next.@"continue"(self.value, .{
-                        .executor = InlineExecutor,
-                    });
+                    self.next.@"continue"(
+                        v,
+                        .{
+                            .executor = InlineExecutor,
+                        },
+                    );
                 }
             };
         }
 
         pub fn materialize(
-            self: @This(),
+            _: @This(),
             continuation: anytype,
         ) Computation(@TypeOf(continuation)) {
             return .{
                 .next = continuation,
-                .value = self.value,
             };
         }
     };
 }
 
-pub fn value(v: anytype) Value(@TypeOf(v)) {
-    return .{ .value = v };
+pub fn value(comptime v: anytype) Value(v) {
+    return .{};
 }
