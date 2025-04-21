3700893186cc6818dd12e6e39ed73f903044c5a6
Andrey Kirpach <andrey.kirpach@gmail.com>
2025-04-21 22:30:07 +0900

    feat(examples): add fiber

---
+++ examples/fiber.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+const zinc = @import("zinc");
+const ThreadPool = zinc.executors.threadPools.Compute;
+const Fiber = zinc.Fiber;
+const assert = std.debug.assert;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        if (gpa.detectLeaks()) {
+            unreachable;
+        }
+    }
+
+    // Create fixed number of "worker threads" at init time.
+    var thread_pool = try ThreadPool.init(2, allocator);
+    const executor = thread_pool.executor();
+    defer thread_pool.deinit();
+    try thread_pool.start();
+    defer thread_pool.stop();
+
+    const Ctx = struct {
+        sum: usize,
+        wait_group: std.Thread.WaitGroup = .{},
+        mutex: Fiber.Mutex = .{},
+        pub fn run(
+            self: *@This(),
+        ) void {
+            for (0..10) |_| {
+                {
+                    // Fibers running on thread pool may access
+                    // shared variable `sum` in parallel.
+                    // Fiber.Mutex provides mutual exclusion without
+                    // blocking underlying thread.
+                    self.mutex.lock();
+                    defer self.mutex.unlock();
+                    self.sum += 1;
+                }
+                // Suspend execution here (allowing for other fibers to be run),
+                // and immediately reschedule self with the Executor.
+                Fiber.yield();
+            }
+            self.wait_group.finish();
+        }
+    };
+    var ctx: Ctx = .{ .sum = 0 };
+    const fiber_count = 4;
+    ctx.wait_group.startMany(fiber_count);
+
+    // Run 4 fibers on 2 threads
+    for (0..fiber_count) |fiber_id| {
+        try Fiber.goWithNameFmt(
+            Ctx.run,
+            .{&ctx},
+            allocator,
+            executor,
+            "Fiber #{}",
+            .{fiber_id},
+        );
+    }
+    // Synchronize Fibers running in a thread pool
+    // with the launching (main) thread.
+    ctx.wait_group.wait();
+}
--- examples/root.zig
+++ examples/root.zig
@@ -1,2 +1,3 @@
 pub const coroutine = "./coroutine.zig";
 pub const threadPool = "./threadPool.zig";
+pub const fiber = "./fiber.zig";
