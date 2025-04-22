e4a365038e4b2df5a344d5e8fd2950e390282447
Andrey Kirpach <andrey.kirpach@gmail.com>
2025-04-22 14:27:27 +0900

    feat(examples): add fiber/channel/select

---
+++ examples/fiber_channel_select.zig
@@ -0,0 +1,78 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
+const zinc = @import("zinc");
+const ThreadPool = zinc.executors.threadPools.Compute;
+const Fiber = zinc.Fiber;
+const Channel = zinc.Fiber.Channel;
+const select = zinc.Fiber.select;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        if (gpa.detectLeaks()) {
+            unreachable;
+        }
+    }
+
+    var thread_pool = try ThreadPool.init(1, allocator);
+    const executor = thread_pool.executor();
+    defer thread_pool.deinit();
+    try thread_pool.start();
+    defer thread_pool.stop();
+
+    const Ctx = struct {
+        channel_usize: Channel(usize) = .{},
+        channel_string: Channel([]const u8) = .{},
+        wait_group: std.Thread.WaitGroup = .{},
+
+        const Self = @This();
+
+        pub fn sendString(ctx: *Self) void {
+            ctx.channel_string.send("456");
+            ctx.wait_group.finish();
+        }
+
+        pub fn receiver(ctx: *Self) void {
+            switch (select(
+                .{
+                    .{ .receive, &ctx.channel_usize },
+                    .{ .receive, &ctx.channel_string },
+                },
+            )) {
+                .@"0" => |_| {
+                    unreachable;
+                },
+                .@"1" => |optional_result_string| {
+                    // null indicates that channel was closed
+                    if (optional_result_string) |result_string| {
+                        assert(std.mem.eql(u8, "456", result_string));
+                    } else unreachable;
+                },
+            }
+            ctx.wait_group.finish();
+        }
+    };
+
+    var ctx: Ctx = .{};
+    ctx.wait_group.startMany(2);
+
+    try Fiber.go(
+        Ctx.receiver,
+        .{&ctx},
+        allocator,
+        executor,
+    );
+
+    try Fiber.go(
+        Ctx.sendString,
+        .{&ctx},
+        allocator,
+        executor,
+    );
+
+    // Synchronize Fibers running in a thread pool
+    // with the launching (main) thread.
+    ctx.wait_group.wait();
+}
--- examples/root.zig
+++ examples/root.zig
@@ -1,3 +1,4 @@
 pub const coroutine = "./coroutine.zig";
 pub const threadPool = "./threadPool.zig";
 pub const fiber = "./fiber.zig";
+pub const fiber_channel_select = "./fiber_channel_select.zig";
