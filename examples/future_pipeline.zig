7a618e3745dce8710610a3a1f24616c19c02315c
Andrey Kirpach <andrey.kirpach@gmail.com>
2025-04-26 22:45:50 +0900

    feat(example): add future_pipeline

---
+++ examples/future_pipeline.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+const zinc = @import("zinc");
+const ThreadPool = zinc.executors.ThreadPools.Compute;
+const future = zinc.future.lazy;
+const assert = std.debug.assert;
+
+pub fn main() !void {
+    // Define asynchronous pipeline
+    // At this point, `p`'s computation does not start executing.
+    // `p` only represents the desired sequence of computations
+    // to be executed some time later, when `p`' is evaluated.
+    // No heap allocations happen at this point.
+    const p = future.pipeline(
+        .{
+            future.constValue(@as(usize, 123)),
+            future.map(
+                struct {
+                    pub fn run(_: ?*anyopaque, in: usize) usize {
+                        return in + 1;
+                    }
+                }.run,
+                null,
+            ),
+        },
+    );
+
+    // ...
+    // do something else (e.g. pass `p` between functions)
+    // ...
+
+    // Evaluate `p`, obtaining the result of the computation pipeline.
+    // no heap allocations happen at this point.
+    // The necessary memory for evaluating `p` and executing
+    // the pipeline's computations is acquired from the stack
+    // at the callsite of `future.get`
+    // This thread is blocked until `p`'s computations complete.
+    const result = future.get(p);
+    assert(result == 124);
+}
--- examples/root.zig
+++ examples/root.zig
@@ -2,3 +2,4 @@ pub const coroutine = "./coroutine.zig";
 pub const threadPool = "./threadPool.zig";
 pub const fiber = "./fiber.zig";
 pub const fiber_channel_select = "./fiber_channel_select.zig";
+pub const future_pipeline = "./future_pipeline.zig";
