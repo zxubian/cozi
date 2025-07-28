666c0d11d14cdfe43e9fd9b79409f9e469fa1d2c
Andrey Kirpach <andrey.kirpach@gmail.com>
2025-07-28 23:31:51 +0700

    fix(await/worker): better handling of std.thread case

--- src/await/worker/thread.zig
+++ src/await/worker/thread.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 const cozi = @import("../../root.zig");
-const Awaiter = cozi.@"await".Awaiter;
-const Worker = cozi.@"await".Worker;
+const Awaiter = cozi.await.Awaiter;
+const Worker = cozi.await.Worker;
+const log = cozi.core.log.scoped(.worker);
 
 threadlocal var name_buf: [std.Thread.max_name_len:0]u8 = [_:0]u8{0} ** std.Thread.max_name_len;
 threadlocal var parking_lot: cozi.fault.stdlike.atomic.Value(u32) = .init(0);
 
 pub inline fn worker(self: *std.Thread) Worker {
-    return cozi.@"await".Worker{
+    return .{
         .ptr = self,
         .type = .thread,
         .vtable = .{
@@ -23,8 +24,12 @@ fn @"suspend"(self: *std.Thread, awaiter: Awaiter) void {
     switch (awaiter.awaitSuspend(worker(self))) {
         .never_suspend => return,
         .always_suspend => {
+            std.debug.print("suspend begin\n", .{});
             if (parking_lot.cmpxchgStrong(0, 1, .seq_cst, .seq_cst) == 0) {
+                std.debug.print("suspend\n", .{});
                 std.Thread.Futex.wait(&parking_lot, 1);
+            } else {
+                std.debug.print("suspend fail\n", .{});
             }
         },
         .symmetric_transfer_next => {
@@ -34,6 +39,7 @@ fn @"suspend"(self: *std.Thread, awaiter: Awaiter) void {
 }
 
 fn @"resume"(_: *std.Thread) void {
+    std.debug.print("resume\n", .{});
     _ = parking_lot.cmpxchgStrong(1, 0, .seq_cst, .seq_cst);
 }
 
@@ -51,6 +57,13 @@ pub fn getName(
     thread: *std.Thread,
     buf: *[std.Thread.max_name_len:0]u8,
 ) ![:0]const u8 {
+    if (thread == invalid_thread_handle) {
+        return try std.fmt.bufPrintZ(
+            buf,
+            "{s}",
+            .{"Unknown"},
+        );
+    }
     const maybe_name: ?[:0]const u8 = blk: {
         const name = thread.getName(buf) catch {
             break :blk null;
@@ -68,6 +81,14 @@ pub fn getName(
     }
 }
 
+const invalid_thread_handle: *std.Thread = @ptrFromInt(
+    std.mem.alignBackwardAnyAlign(
+        usize,
+        std.math.maxInt(u64),
+        @alignOf(std.Thread),
+    ),
+);
+
 pub fn setName(
     _: *std.Thread,
     name: [:0]const u8,
@@ -76,6 +97,15 @@ pub fn setName(
 }
 
 pub fn systemThreadWorker() Worker {
-    // TODO: use std.Thread.current() when it is supported
-    return worker(undefined);
+    log.warn("unknown (system) thread", .{});
+    return .{
+        .type = .thread,
+        .vtable = .{
+            .@"suspend" = @ptrCast(&@"suspend"),
+            .@"resume" = @ptrCast(&@"resume"),
+            .getName = @ptrCast(&getNameBuffered),
+            .setName = @ptrCast(&setName),
+        },
+        .ptr = invalid_thread_handle,
+    };
 }
